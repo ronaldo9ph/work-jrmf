4d77fab3beec2bbe9b3c771d28b95ad22c6988b0
Listing <zhaoxinze@jrmf360.com>
2017-08-10 10:14:37 +0800

    upd http.js

--- src/libs/http.js
+++ src/libs/http.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import qs from 'qs'
 
 axios.defaults.timeout = 5000
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
 axios.defaults.baseURL = 'http://rap.mofang.com/mockjsdata/2/'
 // axios.defaults.baseURL = 'http://192.168.30.177:8080/yilucaifu-openapi/'
 // POST传参序列化
